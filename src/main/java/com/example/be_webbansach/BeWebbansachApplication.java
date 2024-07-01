cc9f208071e819e474939a5d9e4af67a9f257480
thonghtph43253 <hoangthong2105@gmail.com>
2024-07-01 16:11:00 +0700

    Create project

---
+++ src/main/java/com/example/be_webbansach/BeWebbansachApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_webbansach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeWebbansachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeWebbansachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/be_webbansach/BeWebbansachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_webbansach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeWebbansachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
